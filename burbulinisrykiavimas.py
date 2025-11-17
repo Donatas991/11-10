b457571c3be1d6e901fde27f8f469564b0077cda
Donatas991 <m.donrim@gmail.com>
2025-11-17 10:32:55 +0200

    Add files via upload

---
+++ burbulinisrykiavimas.py
@@ -0,0 +1,13 @@
+# nesuprantau starto laustiniu skliausteliu vertes pagrindiniu
+
+from random inport
+
+
+n = int(input("kiek krepsinio zaideju? n="))
+
+a = [randint(170,215) for a in range(n)]
+print("krepsininku ugiai ", a)
+# neprisimenu akmens sortinimo metodo
+for i in rang(n-1)
+    if rik
+
